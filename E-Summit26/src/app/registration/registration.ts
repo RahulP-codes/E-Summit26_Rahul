7e5fd043246e90dbade79f05f27ba707ace4b9ff
RahulP-codes <rahul7pol7@gmail.com>
2025-08-26 20:31:47 +0530

    registration page

---
+++ E-Summit26/src/app/registration/registration.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './registration.html',
+  styleUrls: ['./registration.css']
+})
+export class Registration {
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      firstName: ['', Validators.required],
+      secondName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form Submitted:', this.registrationForm.value);
+      // Add your registration logic here
+    }
+  }
+}
